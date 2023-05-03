80ac06fbe99e28bdb11f48306bd1cceb6400eaef
nahid-dev <nahidv232@gmail.com>
2023-05-04 01:41:53 +0600

    google login done

--- src/Pages/LoginAndRegister/Login.jsx
+++ src/Pages/LoginAndRegister/Login.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProviders";
+import { toast } from "react-hot-toast";
 
 const Login = () => {
+  const [error, setError] = useState("");
+  const { loggedInUser, googleSignIn } = useContext(AuthContext);
+
+  const handleLoginBtn = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    loggedInUser(email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        toast.success("login successfully");
+        form.reset();
+        setError("");
+      })
+      .catch((err) => {
+        setError(err.message);
+        console.log(err.message);
+      });
+  };
+
+  const handleGoogleSignIn = () => {
+    googleSignIn()
+      .then((result) => {
+        const loginUser = result.user;
+        console.log(loginUser);
+        toast.success("Login successfully");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
   return (
     <>
       <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
@@ -11,34 +48,21 @@ const Login = () => {
             alt="Sample image"
           />
         </div>
-        <div className="md:w-1/3 max-w-sm">
+        <form onSubmit={handleLoginBtn} className="md:w-1/3 max-w-sm">
           <div className="text-center md:text-left">
             <label className="mr-1">Sign in with</label>
             <button
+              onClick={handleGoogleSignIn}
               type="button"
-              className="mx-1 h-9 w-9 rounded-full bg-blue-600 hover:bg-blue-700 text-white shadow-[0_4px_9px_-4px_#3b71ca]"
+              className=" h-9 w-9 rounded-full bg-blue-600 hover:bg-blue-700 text-white shadow-[0_4px_9px_-4px_#3b71ca]"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="mx-auto h-3.5 w-3.5"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-              </svg>
+              <FaGoogle className="ms-2"></FaGoogle>
             </button>
             <button
               type="button"
               className="inlne-block mx-1 h-9 w-9 rounded-full bg-blue-600 hover:bg-blue-700 uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca]"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="mx-auto h-3.5 w-3.5"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-              </svg>
+              <FaGithub className="ms-3"></FaGithub>
             </button>
           </div>
           <div className="my-5 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
@@ -48,13 +72,17 @@ const Login = () => {
           </div>
           <input
             className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
-            type="text"
+            type="email"
             placeholder="Email Address"
+            name="email"
+            required
           />
           <input
             className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
             type="password"
             placeholder="Password"
+            name="password"
+            required
           />
           <div className="mt-4 flex justify-between font-semibold text-sm">
             <label className="flex text-slate-500 hover:text-slate-600 cursor-pointer">
@@ -76,6 +104,7 @@ const Login = () => {
               Login
             </button>
           </div>
+          <p className="text-red-500">{error}</p>
           <div className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left">
             Don't have an account?{" "}
             <Link
@@ -85,7 +114,7 @@ const Login = () => {
               Register
             </Link>
           </div>
-        </div>
+        </form>
       </section>
     </>
   );
--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,16 +1,33 @@
 import React, { createContext } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
 import app from "../firebase.config";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProviders = ({ children }) => {
+  // Register
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-  const authInfo = { createUser };
+  // sign In
+  const loggedInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  // google sign in
+  const googleSignIn = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const authInfo = { createUser, loggedInUser, googleSignIn };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
